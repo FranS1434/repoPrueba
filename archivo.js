64ceaae40dc2f81b0f4d422ec5f1edc58478199d
Francisco <fs080700@gmail.com>
2022-04-15 12:18:51 -0300

    commit inicial

---
+++ archivo.js
@@ -0,0 +1,12 @@
+/*primero se debe relacionar el repositorio a la linea de
+comandos de git ingresando a dicha carpeta. con git status
+se corrobora que archivos esta siguiendo git.
+con git add mas el nombre del archivo se relaciona el archivo
+a seguir en git. a git add se le pueden pasar pasar de distintas
+formas los parametros, una es con el nombre del arcivo, otra
+es con ".mas la extension de todos los archivos a agregar
+y por ultimo solo con . para todos los archivos con cualquier
+extension. Tambien se pueden poner los nombres de todos los 
+archivos uno al lado del otro. A ESTO SE LO CONOCE COMO 
+AGREGAR A ESTANCIA DE STAGE */
+console.log("hola mundo");
