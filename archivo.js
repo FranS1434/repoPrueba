a600301fa1ada9f15902c73142ca522e026ca4be
Francisco <fs080700@gmail.com>
2022-04-21 15:03:20 -0300

    ultimo commit

--- archivo.js
+++ archivo.js
@@ -8,7 +8,29 @@ es con ".mas la extension de todos los archivos a agregar
 y por ultimo solo con . para todos los archivos con cualquier
 extension. Tambien se pueden poner los nombres de todos los 
 archivos uno al lado del otro. A ESTO SE LO CONOCE COMO 
-AGREGAR A ESTANCIA DE STAGE */
+AGREGAR A ESTANCIA DE STAGE
+
+renombrar archivo = git mv nombreArchivo nuevoNombre
+despues de esto se tiene que usar git add con los parametros
+de los dos nombre de archivos, el que se va renmbrar mas el
+nuevo nombre.
+al igual que git rm evita tener que llamar a git  add para 
+que se apliquen los cambios
+
+
+elimianr archivo = git rm nombreArchivo. sirve para evitar
+tener que llamar a git add para que se agreguen los cambios
+que se realizaron en el archivo 
+
+mover archivo = mv nombreArchivo desino
+
+IGNORANDO ARCHIVOS
+
+   
+
+
+
+*/
 console.log("hola mundo");
 
 console.log("este este va ser otro cambio al archivo")
